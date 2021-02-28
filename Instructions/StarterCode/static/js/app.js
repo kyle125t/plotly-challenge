b2e337505e82febf687fdc7f86bc0a7ce528f1b6
Kyle Terramoccia <kyleterramoccia@kyles-mbp.myfiosgateway.com>
2021-02-28 18:44:44 -0500

    app.js: Updating file

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,3 +1,4 @@
+// Pull metadata from samples.json file for panel
 function getMetadata(sample) {
   d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
@@ -9,4 +10,32 @@ function getMetadata(sample) {
       panel.append("p").text(`${key}: ${value}`);
     });
   });
+}
+
+// Pull metadata from samples.json to make charts
+function createChart(sample) {
+  d3.json("samples.json").then((data) => {
+    var samples = data.samples;
+    var metaArray = samples.filter(bellySample => bellySample.id == sample);
+    var result = metaArray[0];
+    var ids = result.otu_ids;
+    var labels = result.otu_labels;
+    var values = result.sample_values;
+
+    var bubbleChart = {
+      xaxis: { title: "IDs"},
+      hovermode: "closest"
+      };
+      var bubbleData = [
+        {
+          x: ids,
+          y: values,
+          text: labels,
+          mode: "markers",
+          color: ids,
+          size: values
+        }
+      ];
+      Plotly.plot("bubble", bubbleChart, createChart);
+  })
 }
