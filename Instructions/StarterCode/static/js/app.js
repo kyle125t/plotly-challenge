84ff045542817e0808be80f7e040ee2acd9e0fef
Kyle Terramoccia <kyleterramoccia@kyles-mbp.myfiosgateway.com>
2021-03-02 18:22:25 -0500

    Adding uncleaned code

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,41 +1,88 @@
-// Pull metadata from samples.json file for panel
-function getMetadata(sample) {
-  d3.json("samples.json").then((data) => {
-    var metadata = data.metadata;
-    var metaArray = metadata.filter(bellySample => bellySample.id == sample);
-    var result = metaArray[0];
-    var panel = d3.select("#sample-metadata");
-    panel.html("");
-    Object.entries(result).forEach(([key, value]) => {
-      panel.append("p").text(`${key}: ${value}`);
+function init() {
+    var selection = d3.select("#selDataset");
+  
+    d3.json("./data/samples.json").then((data) => {
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selection.append("option").text(sample).property("value", sample);
+      });
+  
+      const sample1 = sampleNames[0];
+      bellyCharts(sample1);
+      pullMetadata(sample1);
     });
-  });
-}
+  }
 
-// Pull metadata from samples.json to make charts
-function createChart(sample) {
-  d3.json("samples.json").then((data) => {
-    var samples = data.samples;
-    var metaArray = samples.filter(bellySample => bellySample.id == sample);
-    var result = metaArray[0];
+function pullMetadata(sample) {
+    d3.json("./data/samples.json").then((data) => {
+      var metadata= data.metadata;
+      var metaArray= metadata.filter(metaObject => metaObject.id == sample);
+      var result= metaArray[0];
+      var infoPanel = d3.select("#sample-metadata");
+      infoPanel.html("");
+      Object.entries(result).forEach(([key, value]) => {
+        infoPanel.append("h6").text(`${key}: ${value}`);
+      });    
+    });
+  }
+
+
+function bellyCharts(sample) {
+
+  d3.json("./data/samples.json").then((data) => {
+    var samples= data.samples;
+    var metaArray= samples.filter(metaObject => metaObject.id == sample);
+    var result= metaArray[0]
+
     var ids = result.otu_ids;
     var labels = result.otu_labels;
     var values = result.sample_values;
 
-    var bubbleChart = {
-      xaxis: { title: "IDs"},
-      hovermode: "closest"
+    var bubbleLayout = {
+      margin: { t: 0 },
+      xaxis: { title: "IDs" },
+      hovermode: "closest",
       };
+
       var bubbleData = [
-        {
-          x: ids,
-          y: values,
-          text: labels,
-          mode: "markers",
+      {
+        x: ids,
+        y: values,
+        text: labels,
+        mode: "markers",
+        marker: {
           color: ids,
-          size: values
-        }
-      ];
-      Plotly.plot("bubble", bubbleChart, createChart);
-  })
+          size: values,
+          }
+      }
+    ];
+
+    Plotly.plot("bubble", bubbleData, bubbleLayout);
+
+     var barLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      margin: { t: 30, l: 150 }
+    };
+
+    var barData =[
+      {
+        y: ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+        x: values.slice(0,10).reverse(),
+        text: labels.slice(0,10).reverse(),
+        type: "bar",
+        orientation: "h"
+
+      }
+    ];
+
+    Plotly.newPlot("bar", barData, barLayout);
+  });
 }
+
+function optionChanged(newSample) {
+  bellyCharts(newSample);
+  pullMetadata(newSample);
+}
+
+// Initialize main function
+init();
